dcee113521737e6b805dfb411c3d606d7fbfa457
Alex Spedding <alex@Alexs-MacBook-Pro-16.local>
2024-07-31 18:04:00 +0100

    Streaming endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 #[macro_use]
 extern crate rocket;
 
+use rand::distributions::{Alphanumeric, DistString};
+use rand::Rng;
+use rocket::response::stream::TextStream;
+use rocket::tokio::time::{self, Duration};
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, Docker!"
 }
 
+#[get("/word-stream")]
+fn word_stream() -> TextStream![String] {
+    TextStream! {
+        let mut interval = time::interval(Duration::from_secs(1));
+        loop {
+            let ret = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+            yield ret;
+            interval.tick().await;
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, word_stream])
 }
